0ec7aba8056712444e63d399a9ef4ba350b35366
kimhieu-dev <nguyenkimhieu.dev@gmail.com>
2026-04-21 17:04:06 +0700

    feat(cart): add Cart service

---
+++ src/main/java/com/nkh/ecommercebackend/service/impl/CartServiceImpl.java
@@ -0,0 +1,12 @@
+package com.nkh.ecommercebackend.service.impl;
+
+import com.nkh.ecommercebackend.repository.CartRepo;
+import com.nkh.ecommercebackend.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CartServiceImpl implements CartService {
+    private final CartRepo cartRepo;
+}

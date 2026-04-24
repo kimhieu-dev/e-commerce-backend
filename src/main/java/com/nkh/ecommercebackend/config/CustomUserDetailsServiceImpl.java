db7a75d68627c7a8806c7028a5a594f3815543a3
kimhieu-dev <nguyenkimhieu.dev@gmail.com>
2026-04-24 18:05:25 +0700

    feat(auth): add basic auth

---
+++ src/main/java/com/nkh/ecommercebackend/config/CustomUserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.nkh.ecommercebackend.config;
+
+import com.nkh.ecommercebackend.repository.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepo userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<com.nkh.ecommercebackend.entity.User> userOptional = userRepo.findByUsername(username);
+        if(userOptional.isEmpty()) {
+            throw new UsernameNotFoundException("Username not found");
+        }
+//        String role = "ROLE_USER";
+//        SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + role);
+        return new User(username,userOptional.get().getPassword(), List.of());
+
+    }
+}
--- src/main/java/com/nkh/ecommercebackend/config/WebSecurityConfig.java
+++ src/main/java/com/nkh/ecommercebackend/config/WebSecurityConfig.java
@@ -1,11 +1,27 @@
 package com.nkh.ecommercebackend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 
-@EnableWebSecurity
+
 @Configuration
 public class WebSecurityConfig {
-
+    @Bean
+    public SecurityFilterChain configSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+                        authorizationManagerRequestMatcherRegistry
+                                .requestMatchers(HttpMethod.POST, "/api/v1/users")
+                                .permitAll()
+                                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 }

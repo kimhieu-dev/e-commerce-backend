62dc80f2cb85343532258c5f24e5fa5e33486de2
kimhieu-dev <nguyenkimhieu.dev@gmail.com>
2026-04-24 18:07:42 +0700

    feat: add handle validation exception

--- src/main/java/com/nkh/ecommercebackend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nkh/ecommercebackend/exception/GlobalExceptionHandler.java
@@ -3,12 +3,20 @@ package com.nkh.ecommercebackend.exception;
 import com.nkh.ecommercebackend.dto.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> handleValidationException(MethodArgumentNotValidException methodArgumentNotValidException){
+        String key = methodArgumentNotValidException.getFieldError().getDefaultMessage();
+        ErrorCode errorCode = ErrorCode.valueOf(key);
+        return ResponseEntity.status(errorCode.getHttpStatus()).body(ApiResponse.error(errorCode));
+    }
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ApiResponse<?>> handleBusinessException(BusinessException businessException) {

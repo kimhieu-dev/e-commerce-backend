3021c96076ec58860241e0747fb9bf5675be384e
kimhieu-dev <nguyenkimhieu.dev@gmail.com>
2026-05-06 09:54:25 +0700

    feat(carrier): implement get carriers

---
+++ src/main/java/com/nkh/ecommercebackend/controller/CarrierController.java
@@ -0,0 +1,25 @@
+package com.nkh.ecommercebackend.controller;
+
+import com.nkh.ecommercebackend.dto.response.BaseResponse;
+import com.nkh.ecommercebackend.dto.response.CarrierRes;
+import com.nkh.ecommercebackend.service.CarrierService;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/carriers")
+@RequiredArgsConstructor
+public class CarrierController {
+    private CarrierService carrierService;
+
+    @GetMapping
+    public BaseResponse<List<CarrierRes>>  getCarriers() {
+        List<CarrierRes> response = carrierService.getCarriers();
+        return BaseResponse.success(response);
+    }
+}
